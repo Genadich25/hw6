86a459c3e8336fd943351cc0873e9041081efb62
pin20 <pin200048@gmail.com>
2022-01-13 20:00:05 +0300

    Готовое дз

--- src/ru/lookBag/Main.java
+++ src/ru/lookBag/Main.java
@@ -5,19 +5,50 @@ public class Main {
     public static void main(String[] args) {
     //1 задача
 
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String secondName = "Ivanov";
+        String fullName = secondName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника — " + fullName);
 
+        System.out.println();
 
     //2 задача
 
+        String upperCaseFullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + upperCaseFullName);
 
+        System.out.println();
 
     //3 задача
 
+        String[] wordsFullName = fullName.split(" ");
+        System.out.print("Данные ФИО сотрудника для административного отдела — ");
+        for (int i = 0; i < wordsFullName.length; i++) {
+            if(i != wordsFullName.length - 1){
+                System.out.print(wordsFullName[i] + "; ");
+            } else {
+                System.out.println(wordsFullName[i]);
+            }
+        }
 
+        System.out.println();
 
     //4 задача
 
+        fullName = "Иванов Семён Семёнович";
+        char[] charFullName = fullName.toCharArray();
+        System.out.print("Данные ФИО сотрудника — ");
+        for (int i = 0; i < charFullName.length; i++) {
+            if(charFullName[i] != 'ё'){
+                System.out.print(charFullName[i]);
+            } else {
+                charFullName[i] = 'е';
+                System.out.print(charFullName[i]);
+            }
+        }
 
+        System.out.println();
 
     }
 }
