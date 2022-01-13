e4209a7ac547995bbc2adaf92ada41cea9b72f35
pin20 <pin200048@gmail.com>
2022-01-13 19:21:22 +0300

    Инициализация ветки

--- src/ru/lookBag/Main.java
+++ src/ru/lookBag/Main.java
@@ -3,6 +3,21 @@ package ru.lookBag;
 public class Main {
 
     public static void main(String[] args) {
+    //1 задача
+
+
+
+    //2 задача
+
+
+
+    //3 задача
+
+
+
+    //4 задача
+
+
 
     }
 }
